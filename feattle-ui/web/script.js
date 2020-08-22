b40a72a423e7c50bba5d8daabd9bfcd95d0a7d3f
Guilherme Souza <19gui94@gmail.com>
2020-08-22 20:49:49 +0200

    Implement editor for Optional and refactor others

--- feattle-ui/web/script.js
+++ feattle-ui/web/script.js
@@ -1,83 +1,133 @@
 'use strict'
 
 class FeatureEditor {
-    constructor(editorEl, outputEl) {
+    constructor(editorEl) {
         this.editorEl = editorEl
-        this.outputEl = outputEl
-        this.sourceEl = null
 
         this.format = JSON.parse(editorEl.attr('data-format'))
-        this.value = JSON.parse(editorEl.attr('data-value'))
+        this.initialValue = JSON.parse(editorEl.attr('data-value'))
 
-        if (this.format.tag == 'Bool') {
-            this._bool()
-        } else if (this.format.tag == 'Integer') {
-            this._number(true)
-        } else if (this.format.tag == 'Float') {
-            this._number(false)
-        } else if (this.format.tag == 'String' && this.format.content.tag == 'Any') {
-            this._string()
-        } else if (this.format.tag == 'String' && this.format.content.tag == 'Pattern') {
-            this._string(this.format.content.content)
-        } else if (this.format.tag == 'String' && this.format.content.tag == 'Choices') {
-            this._choices(this.format.content.content)
+        this.sourceEl = null
+        this.getValue = null
+
+        if (this.format.tag === 'Bool') {
+            this._prepareBool()
+        } else if (this.format.tag === 'Integer') {
+            this._prepareNumber(true)
+        } else if (this.format.tag === 'Float') {
+            this._prepareNumber(false)
+        } else if (this.format.tag === 'String' && this.format.content.tag === 'Any') {
+            this._prepareString()
+        } else if (this.format.tag === 'String' && this.format.content.tag === 'Pattern') {
+            this._prepareString(this.format.content.content)
+        } else if (this.format.tag === 'String' && this.format.content.tag === 'Choices') {
+            this._prepareChoices(this.format.content.content)
+        } else if (this.format.tag === 'Optional') {
+            this._prepareOptional(this.format.content)
         } else {
             this._other()
         }
-
-        this.editorEl.append(this.sourceEl)
-        this.sourceEl.change(() => {
-            this._setValue(this.sourceEl.val())
-        })
     }
 
-    _bool() {
-        this.sourceEl = $('<select>', {
-            'class': 'custom-select',
-            append: [
-                $('<option>', {
-                    value: 'true',
-                    selected: this.value,
-                    text: 'true'
-                }),
-                $('<option>', {
-                    value: 'false',
-                    selected: !this.value,
-                    text: 'false'
-                }),
-            ]
-        })
+    _prepareBool() {
+        this.sourceEl = this._newSwitch(this.editorEl, 'Value', this.initialValue)
+        this.getValue = () => this.sourceEl.prop('checked')
     }
 
-    _number(isInteger) {
+    _prepareNumber(isInteger) {
         this.sourceEl = $('<input>', {
+            'class': 'form-control',
             type: 'number',
             step: isInteger ? '' : 'any',
-            val: this.value
+            val: this.initialValue
         })
+        this.editorEl.append(this.sourceEl)
+        this.getValue = () => Number(this.sourceEl.val())
     }
 
-    _string(pattern) {
+    _prepareString(pattern) {
         this.sourceEl = $('<input>', {
-            pattern: pattern || '',
-            val: this.value
+            'class': 'form-control',
+            pattern: pattern,
+            val: this.initialValue
         })
+        this.editorEl.append(this.sourceEl)
+        this.getValue = () => this.sourceEl.val()
     }
 
-    _choices(choices) {
-        this.sourceEl = $('<select>', {
-            'class': 'custom-select',
-            append: choices.map(choice => $('<option>', {
-                value: choice,
-                selected: choice == this.value,
-                text: choice
-            }))
-        })
+    _prepareChoices(choices) {
+        let radioGroupId = String(Math.random())
+        this.sourceEl = $(choices.map(choice => {
+            let radioId = String(Math.random())
+            return $('<div>', {
+                'class': 'custom-control custom-radio',
+                append: [
+                    $('<input>', {
+                        type: 'radio',
+                        name: radioGroupId,
+                        id: radioId,
+                        'class': 'custom-control-input',
+                        value: choice,
+                        checked: choice === this.initialValue
+                    }),
+                    $('<label>', {
+                        'class': 'custom-control-label',
+                        'for': radioId,
+                        text: choice
+                    })
+                ]
+            }).get(0)
+        }))
+        this.editorEl.append(this.sourceEl)
+        this.getValue = () => $('input:checked', this.sourceEl).val()
     }
 
-    _setValue(value) {
-        console.log('Change value to', JSON.stringify(value))
-        this.value = value
-        this.outputEl.val(JSON.stringify(value))
+    _prepareOptional(innerFormat) {
+        this.switchEl = this._newSwitch(this.editorEl, 'Define some value', this.initialValue !== null)
+
+        this.sourceEl = $('<div>', {
+            'class': 'py-2',
+            attr: {
+                'data-format': JSON.stringify(innerFormat),
+                'data-value': JSON.stringify(this.initialValue)
+            }
+        })
+        this.editorEl.append(this.sourceEl)
+        this.innerEditor = new FeatureEditor(this.sourceEl)
+
+        this.switchEl.change(() => {
+            if (this.switchEl.prop('checked')) {
+                this.sourceEl.show()
+            } else {
+                this.sourceEl.hide()
+            }
+        })
+        if (this.initialValue === null) {
+            this.sourceEl.hide()
+        }
+
+        this.getValue = () => this.switchEl.prop('checked') ? this.innerEditor.getValue() : null
+    }
+
+    _newSwitch(rootEl, label, initialValue) {
+        let inputId = String(Math.random())
+        let switchEl = $('<input>', {
+            type: 'checkbox',
+            'class': 'custom-control-input',
+            id: inputId,
+            checked: initialValue
+        })
+        rootEl.append($('<div>', {
+            'class': 'custom-control custom-switch',
+            append: [
+                switchEl,
+                $('<label>', {
+                    'class': 'custom-control-label',
+                    text: label,
+                    'for': inputId
+                })
+            ]
+        }))
+        return switchEl
     }
 }
