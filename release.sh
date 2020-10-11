d575b9bb1ffefa7ced22a8633a4076ffd0b02192
Guilherme <sitegui@sitegui.com.br>
2020-10-11 23:30:04 +0200

    Add versions for subpackages

--- release.sh
+++ release.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -e
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <version>"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <package> <version>"
     exit 1
 fi
 
-VERSION=$1
+PACKAGE=$1
+VERSION=$2
 
-# cargo install cargo-readme
-for PACKAGE in feattle-core feattle-sync feattle-ui; do
-  cargo readme --project-root $PACKAGE --template ../README.tpl > $PACKAGE/README.md
-  sed -i "s/^version = \".*\"$/version = \"$VERSION\"/" $PACKAGE/Cargo.toml
-done
+cargo readme --project-root "$PACKAGE" --template ../README.tpl > "$PACKAGE/README.md"
+sed -i "s/^version = \".*\"$/version = \"$VERSION\"/" "$PACKAGE/Cargo.toml"
+cargo publish --manifest-path "$PACKAGE/Cargo.toml"
